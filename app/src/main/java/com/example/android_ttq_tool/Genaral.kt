1579da03a3dd38ba200c1390eeaedac5c11b27db
Mini Lemon <44203733+tothanhquy@users.noreply.github.com>
2023-02-14 23:20:01 +0700

    second

--- app/src/main/java/com/example/android_ttq_tool/Genaral.kt
+++ app/src/main/java/com/example/android_ttq_tool/Genaral.kt
@@ -4,33 +4,33 @@ import android.app.Activity
 import android.view.Window
 import android.view.WindowManager
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class Genaral {
 	companion object Static{
-		 fun setThemeAndBackground(activity: Activity, background: ImageView){
+		 fun setThemeAndBackground(activity: AppCompatActivity , background: ImageView){
 			val rightNow = Calendar.getInstance()
 			val currentHourIn24Format: Int =rightNow.get(Calendar.HOUR_OF_DAY)
-			if(currentHourIn24Format >= 18 || currentHourIn24Format <= 6 ){
+			if(currentHourIn24Format >= 18 || currentHourIn24Format < 6 ){
 				activity.setTheme(R.style.AppThemeFullscreenNight)
-				background.setBackgroundResource(R.drawable.background_original_night)
-			}else if(currentHourIn24Format <= 12 ){
-				activity.setTheme(R.style.AppThemeFullscreenMorning)
-				background.setBackgroundResource(R.drawable.background_original_morning)
-			}else if(currentHourIn24Format >= 12 ) {
-				activity.setTheme(R.style.AppThemeFullscreenAfternoon)
-				background.setBackgroundResource(R.drawable.background_original_afternoon)
-			}else{
-				//default
-				activity.setTheme(R.style.AppThemeFullscreen)
-				background.setBackgroundResource(R.drawable.background5)
+				background.setBackgroundResource(R.drawable.background_nigh_2_finaly)
+				
+			}else {
+				activity.setTheme(R.style.AppThemeFullscreenDay)
+				background.setBackgroundResource(R.drawable.background_day)
+			
 			}
 		}
-		fun setFullScreen(activity: Activity){
+		fun setFullScreen(activity: AppCompatActivity){
 			activity.requestWindowFeature(Window.FEATURE_NO_TITLE);
 			activity.getWindow().setFlags(
 				WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		}
+	
+		fun hideTitle(activity: AppCompatActivity){
+			activity.supportActionBar?.hide()
 		}
 	}
 	
--- app/src/main/java/com/example/android_ttq_tool/MainActivity.kt
+++ app/src/main/java/com/example/android_ttq_tool/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android_ttq_tool
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +11,10 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState : Bundle?) {
 		super.onCreate(savedInstanceState)
 		Genaral.setFullScreen(this)
+		Genaral.hideTitle(this)
+		setContentView(R.layout.activity_main)
 		val backgroundImageView:ImageView = findViewById<ImageView>(R.id.mainActivityImageViewBackground)
 		Genaral.setThemeAndBackground(this, backgroundImageView)
-		setContentView(R.layout.activity_main)
 	}
 	
 	
